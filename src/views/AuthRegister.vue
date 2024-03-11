0348e7ef37a4a196e8c4d9efedd91821bc5714c9
Don-Tun <val.neerg@gmail.com>
2024-03-11 18:04:24 +0100

    changed register to only take email and calling /auth/email instead of /auth/register

--- src/views/AuthRegister.vue
+++ src/views/AuthRegister.vue
@@ -3,12 +3,8 @@
     <HeaderBar name="Register" />
     <ion-content>
       <form id="form" @submit.prevent="register">
-        <ion-input placeholder="First name" v-model="user.firstname"></ion-input>
-        <ion-input placeholder="Last name" v-model="user.lastname"></ion-input>
         <ion-input placeholder="Email" v-model="user.email"></ion-input>
-        <ion-input type="password" placeholder="Password" v-model="user.password"></ion-input>
-        <ion-input type="password" placeholder="Confirm Password" v-model="user.confirmPassword"></ion-input>
-        <ion-button type="submit" expand="block" shape="round">Register</ion-button>
+        <ion-button type="submit" expand="block" shape="round">Send Confirmation Email</ion-button>
         <p>Already have an account? <router-link to="/auth/login">Login</router-link></p>
       </form>
     </ion-content>
@@ -25,33 +21,16 @@ import { useRouter } from 'vue-router';
 const router = useRouter();
 
 const user = ref({
-  firstname: '',
-  lastname: '',
   email: '',
-  password: '',
-  confirmPassword: ''
 });
 
 const register = async () => {
-  if (user.value.password !== user.value.confirmPassword) {
-    alert("Passwords do not match");
-    return;
-  }
   try {
-    await axios.post("http://localhost:8080/api/v1/auth/signup", {
-      firstname: user.value.firstname,
-      lastname: user.value.lastname,
+    await axios.post("http://localhost:8080/api/v1/auth/email", {
       email: user.value.email,
-      password: user.value.password
     });
 
-    const loginResponse = await axios.post("http://localhost:8080/api/v1/auth/signin", {
-      email: user.value.email,
-      password: user.value.password
-    });
-    localStorage.setItem('accessToken', loginResponse.data.accessToken);
-    localStorage.setItem('refreshToken', loginResponse.data.refreshToken);
-    await router.push('/tabs/home');
+    await router.push('/auth/login');
 
   } catch (error:unknown) {
     // Handle error, e.g., display a message to the user
@@ -74,10 +53,12 @@ const register = async () => {
 <style scoped>
 ion-content {
   #form {
-    place-content: center;
-    padding-top: 10rem;
-    padding-left: 30px;
-    padding-right: 30px
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0 30px;
+    height: 100%;
   }
 }
 </style>
