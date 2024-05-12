0fffcb9077df374866bca7021ff1c42532aff476
AFugl <s215207@student.dtu.dk>
2024-05-13 00:13:35 +0200

    Implemented filtering and search for gallery feature

--- src/views/TabAttendeeDetail.vue
+++ src/views/TabAttendeeDetail.vue
@@ -45,7 +45,7 @@
 import { reactive, onMounted} from 'vue';
 import { useRouter, useRoute} from 'vue-router';
 import axios from 'axios';
-import { IonPage, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonAvatar } from '@ionic/vue';
 
 
 const router = useRouter();
