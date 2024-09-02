02a3858cbcff77a4ae2fb62446e509e45eacf5de
Andrea Burattin <andrea.burattin@gmail.com>
2024-09-02 10:21:05 +0200

    Added clickable chip for authors

--- src/views/gallery/TabGallerySingleImage.vue
+++ src/views/gallery/TabGallerySingleImage.vue
@@ -1,21 +1,24 @@
 <template>
   <ion-page>
-    <HeaderBar name="" />
     <ion-header>
       <ion-toolbar>
         <ion-buttons slot="start">
-          <ion-back-button defaultHref="/tabs/images/myGallery"></ion-back-button>
+          <ion-back-button defaultHref="/tabs/images"></ion-back-button>
         </ion-buttons>
         <ion-title>{{ imagePath }}</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-img
-          :src="image"
-      ></ion-img>
+    <ion-content>
       <ion-card>
+        <img :src="image" />
         <ion-card-content class="Published-text">
-          Published by: {{ imageData.imageAuthor }}
+          Published by:
+          <ion-chip :router-link="`/attendee/${imageData.authorId}`" >
+            <ion-avatar>
+              <img :src="imageData.imageAuthorAvatar || 'https://ionicframework.com/docs/img/demos/avatar.svg'" alt="Profile picture" />
+            </ion-avatar>
+            <ion-label>{{ imageData.imageAuthor }}</ion-label>
+          </ion-chip>
         </ion-card-content>
         <ion-card-content class="Likes-text">
           Likes: {{ imageData.imageLikes }}
@@ -30,7 +33,20 @@
 </template>
 
 <script setup lang="ts">
-import {IonPage, IonContent, IonImg, IonButtons, IonBackButton, IonIcon, IonCard, IonCardContent, IonTitle, IonToolbar, IonHeader, } from "@ionic/vue";
+import {
+  IonPage,
+  IonContent,
+  IonImg,
+  IonButtons,
+  IonBackButton,
+  IonIcon,
+  IonCard,
+  IonCardContent,
+  IonTitle,
+  IonToolbar,
+  IonHeader,
+  IonAvatar, IonLabel, IonChip,
+} from "@ionic/vue";
 import {onMounted, ref} from "vue";
 import {useRoute} from "vue-router";
 import HeaderBar from "@/components/HeaderBar.vue";
@@ -43,7 +59,9 @@ const route = useRoute();
 const imageData = ref({
   imageAuthor: '',
   imageLikes: 0,
-  imageIsLiked: false
+  imageIsLiked: false,
+  authorId: 0,
+  imageAuthorAvatar: ''
 });
 const image = ref('');
 const imagePath = route.params.imageId as string;
@@ -59,6 +77,8 @@ const getImageData = async (filepath:string) => {
     imageData.value.imageAuthor = response.data.imageAuthor;
     imageData.value.imageLikes = response.data.imageLikes;
     imageData.value.imageIsLiked = response.data.hasLiked;
+    imageData.value.authorId = response.data.authorId;
+    imageData.value.imageAuthorAvatar = await getAvatarImage(response.data.authorId);
 
   } catch (e) {
     console.error('Error fetching image data:', e);
@@ -87,28 +107,37 @@ const changeLikeStatus = async () => {
   }
 }
 
+const getAvatarImage = async (id:number) => {
+  try {
+    const response = await axios.get(`https://localhost:8080/api/v1/account/getProfilePicture/${id}`, {
+      headers: { Authorization: `Bearer ${token.value}` },
+      params: {
+        format: 'webp'
+      },
+      responseType: 'blob'  // This tells axios to expect a binary response instead of JSON
+    });
+    if (response.data.size === 0) {
+      return '';
+    }
+    return URL.createObjectURL(response.data);  // Convert the blob to a URL and return it
+  } catch (error) {
+    console.error("Error fetching image:", error);
+    return '';  // Return an empty string or a default image path in case of error
+  }
+}
+
 </script>
 
 <style scoped>
 .like-icon{
   font-size: 32px;
 }
-ion-card{
-  background-color: transparent;
-  margin-top: -2%;
-  margin-left: -1%;
-}
 
 .Published-text{
   font-size: 18px;
-
 }
 .Likes-text{
   font-size: 16px;
   margin-top: -8%;
-}
-.like-icon{
-  color: #3880ff;
-  margin-top: -7%;
 }
 </style>
