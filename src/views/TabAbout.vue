c66bd96e82050a506eec51f5ad83d7a133e39567
Andrea Burattin <andrea.burattin@gmail.com>
2024-09-02 10:21:14 +0200

    Updated about message

--- src/views/TabAbout.vue
+++ src/views/TabAbout.vue
@@ -4,7 +4,8 @@
 
     <ion-content id="main-content" :fullscreen="true">
       <div class="ion-padding">
-        <p>This application has been developed at the Technical University of Denmark (DTU) by Anders Fuglsang, Sigurd Valentin Flach de Neergaard during their BSc project entitled <em>"<a href="https://findit.dtu.dk/en/catalog/668f22de1713b1dc48f78e49" target="_blank">Development of web application with robustness guarantees</a>"</em>.</p>
+        <p>The original version of this application has been developed at the Technical University of Denmark (DTU) by Anders Fuglsang, Sigurd Valentin Flach de Neergaard during their BSc project entitled <em>"<a href="https://findit.dtu.dk/en/catalog/668f22de1713b1dc48f78e49" target="_blank">Development of web application with robustness guarantees</a>"</em>. The application has then been updated and extended by Andrea Burattin and Giovanni Meroni.</p>
+        <p>For questions, please send an email to Andrea Burattin andbur@dtu.dk.</p>
       </div>
     </ion-content>
   </ion-page>
