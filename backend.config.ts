15ef25fbdab6c905c6c2ccad255c848f8df1013c
Andrea Burattin <andrea.burattin@gmail.com>
2024-09-04 16:54:34 +0200

    Tentative deployment on Azure

--- backend.config.ts
+++ backend.config.ts
@@ -2,7 +2,8 @@ type QueryParams = Record<string, string>;
 
 const backend = {
     config: {
-        url: 'https://localhost:8080/api/v1/'
+        url: 'https://icpm-app-backend.azurewebsites.net/api/v1/'
+        // url: 'https://localhost:8080/api/v1/'
     },
     construct: function(endpoint: string, queryParams: QueryParams = {}): string {
         const url = new URL(this.config.url + endpoint);
