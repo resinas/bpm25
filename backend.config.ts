a71bee1198c27782752b08614122014842051c66
Andrea Burattin <andrea.burattin@gmail.com>
2024-09-10 16:13:12 +0200

    😴 bug

--- backend.config.ts
+++ backend.config.ts
@@ -2,7 +2,7 @@ type QueryParams = Record<string, string>;
 
 const backend = {
     config: {
-        url: 'https://api-icpm.compute.dtu.dk//api/v1/'
+        url: 'https://api-icpm.compute.dtu.dk/api/v1/'
     },
     construct: function(endpoint: string, queryParams: QueryParams = {}): string {
         const url = new URL(this.config.url + endpoint);
