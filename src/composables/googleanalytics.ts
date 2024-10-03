d69c14cad2624c96722d095858e115201baeff56
AFugl <fuglsang.anders@gmail.com>
2024-10-03 11:38:32 +0200

    Change eventType and eventLabel to get a more descriptive path exploration

--- src/composables/googleanalytics.ts
+++ src/composables/googleanalytics.ts
@@ -1,13 +1,19 @@
 export const googleanalytics = () => {
 
-    const trackButtonClick = (labelString: string, categoryString: string, eventType: string) => {
+    const trackButtonClick = (eventType: string, categoryString: string,labelString: string ) => {
         if (window.gtag) {
             window.gtag('event', `${eventType}`, {
                 event_category: `${categoryString}`,
                 event_label: `${labelString}`,
+
+                page_title: document.title,
+                page_path: window.location.pathname,
+                device_type: window.innerWidth <= 768 ? 'mobile' : 'desktop'
             });
         }
     };
+
+
 
     return {
         trackButtonClick
